db6bf122de308e8a5e59a984f6a6ccfffd9fe129
Ryan Gray <rgray2@mail.usf.edu>
2018-08-30 23:50:55 -0400

    Implemented load_connections() function
    Appears to work as desired; All attempted test cases passed.

--- Assignment 1 - DFS/Network.cpp
+++ Assignment 1 - DFS/Network.cpp
@@ -4,96 +4,127 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
-//did it work?
-
 using namespace std;
 
 class Node
 {
-  public:
-    Node();
-    Node(string name, float x, float y) {
-      this->name = name;
-      this->x = x;
-      this->y = y;
-    };
-    float dist(Node *neighbor);
-    // gets neighbor with smallest alphanumeric name, returns null if no options
-    Node * get_available_neighbor();
-    string get_name() { return name; };
-    float get_x() { return x; };
-    float get_y() { return y; };
-    bool is_visited() { return visited; };
-    void set_visited() { visited = true; };
-    vector<Node *> get_neighbors() { return neighbors; };
-    void set_neighbors(vector<Node *> neighbors) { this->neighbors = neighbors; };
-  private:
-    string name;
-    float y;
-    float x;
-    bool visited = false;
-    vector<Node *> neighbors;
+public:
+	Node();
+	Node(string name, float x, float y) {
+		this->name = name;
+		this->x = x;
+		this->y = y;
+	};
+	float dist(Node *neighbor);
+	// gets neighbor with smallest alphanumeric name, returns null if no options
+	Node * get_available_neighbor();
+	string get_name() { return name; };
+	float get_x() { return x; };
+	float get_y() { return y; };
+	bool is_visited() { return visited; };
+	void set_visited() { visited = true; };
+	vector<Node *> get_neighbors() { return neighbors; };
+	void set_neighbors(vector<Node *> neighbors) { this->neighbors = neighbors; };
+private:
+	string name;
+	float y;
+	float x;
+	bool visited = false;
+	vector<Node *> neighbors;
 };
 
 class Network
 {
-  public:
-    Network() {};
-    void add_node(Node *node) {
-      nodes[node->get_name()] = node;
-    };
-    void find_path(); // dfs
-    void set_start_node(Node *node);
-    void set_end_node(Node *node);
-    Node * get_start_node();
-    Node * get_end_node();
-    map<string, Node *> get_nodes() { return nodes; };
-  private:
-    map<string, Node *> nodes;
-    stack<Node*> path;
-    const Node *start_node;
-    const Node *end_node;
-    // distance should be computed at the very end during printing instead of 
-    // every time we travel to a new node. Should be more efficient.
-    float total_distance;
+public:
+	Network() {};
+	void add_node(Node *node) {
+		nodes[node->get_name()] = node;
+	};
+	void find_path(); // dfs
+	void set_start_node(Node *node);
+	void set_end_node(Node *node);
+	Node * get_start_node();
+	Node * get_end_node();
+	map<string, Node *> get_nodes() { return nodes; };
+private:
+	map<string, Node *> nodes;
+	stack<Node*> path;
+	const Node *start_node;
+	const Node *end_node;
+	// distance should be computed at the very end during printing instead of 
+	// every time we travel to a new node. Should be more efficient.
+	float total_distance;
 };
 
 struct NetworkIO
 {
-  static void load_locations(Network network) {
-    ifstream file;
-    string line;
-    file.open("locations.txt");
-    if (!file.is_open()) {
-      cout << "Failure to open locations.txt" << endl;
-      return;
-    }
-    while(getline(file, line) && line != "END") {
-      istringstream iss(line);
-      string name;
-      int x, y;
-      iss >> name;
-      iss >> x;
-      iss >> y;
-      network.add_node(new Node(name, x, y));
-    }
-    file.close();
-  };
-  static void load_connections(Network network) {};
-  static Node * set_start_node();
-  static Node * set_end_node();
-  static void print_path(Network network);
+	static void load_locations(Network* network) {
+		ifstream file;
+		string line;
+		file.open("locations.txt");
+		if (!file.is_open()) {
+			cout << "Failure to open locations.txt" << endl;
+			return;
+		}
+		while (getline(file, line) && line != "END") {
+			istringstream iss(line);
+			string name;
+			int x, y;
+			iss >> name;
+			iss >> x;
+			iss >> y;
+			network->add_node(new Node(name, x, y));
+		}
+		file.close();
+	};
+
+	static void load_connections(Network* network) {
+		ifstream file;
+		string line;
+		file.open("connections.txt");
+		if (!file.is_open()) {
+			cout << "Failure to open connections.txt" << endl;
+			return;
+		}
+		while (getline(file, line) && line != "END") {
+			istringstream iss(line);
+			string name;
+			int num_neighbors;
+			iss >> name;
+			iss >> num_neighbors;
+
+			Node* n = network->get_nodes().at(name);
+			vector<Node*> node_neighbors;
+
+			for (int i = 0; i < num_neighbors; i++) {
+				string str;
+				iss >> str;
+
+				//find node that matches name "str" and add to node_neighbors vector
+				Node* neighbor = network->get_nodes().at(str);
+				node_neighbors.push_back(neighbor);
+			}
+
+			//add the node_neighbors vector to the current node on input file
+			network->get_nodes().at(name)->set_neighbors(node_neighbors);
+		}
+		file.close();
+	};
+
+	static void load_connections(Network network) {};
+	static Node * set_start_node();
+	static Node * set_end_node();
+	static void print_path(Network network);
 };
 
 int main() {
-  Network network;
-  NetworkIO::load_locations(network);
-  // NetworkIO::load_connections(network);
-  // network.set_start_node(NetworkIO::set_start_node());
-  // network.set_end_node(NetworkIO::set_end_node());
-  // network.find_path();
-  // NetworkIO::print_path(network);
-  cout << "Press Enter to Continue";
-  cin.ignore();
+	Network network;
+	NetworkIO::load_locations(&network);
+	NetworkIO::load_connections(&network);
+	// network.set_start_node(NetworkIO::set_start_node());
+	// network.set_end_node(NetworkIO::set_end_node());
+	// network.find_path();
+	// NetworkIO::print_path(network);
+	cout << "Press Enter to Continue";
+	cin.ignore();
 }
