0e4d01bc14b0b018ff65faf6896e0d302ebfe5ad
Ryan Gray <rgray2@mail.usf.edu>
2018-09-24 17:55:28 -0400

    cleaner version from last merge

--- Project 1 - A-star/Network.cpp
+++ Project 1 - A-star/Network.cpp
@@ -424,13 +424,10 @@ struct NetworkIO
 		stack<Node*> path_copy = network->get_pq().top()->path;
 		stack<Node*> path_result;
 		double total_distance = 0.0;
-		int cities_count = 0;
 
-		if (path_copy.empty()) {
+		if (path_copy.empty()) 
 			cout << "No path found." << endl; 
-		}
-		else cities_count++;
-
+		
 		//copy the stack from path_copy to path_result
 		while (!path_copy.empty()) {
 			path_result.push(path_copy.top());
@@ -442,8 +439,8 @@ struct NetworkIO
 
 		while (!path_result.empty()) {
 			cout << current->get_name() << " to " << path_result.top()->get_name() << ": length = ";
+
 			if (network->get_fewest_cities()) {
-				cities_count++;
 				cout << "1" << endl;
 			}
 			else {
@@ -455,8 +452,9 @@ struct NetworkIO
 			current = path_result.top();
 			path_result.pop();
 		}
+
 		if (network->get_fewest_cities())
-			cout << "\nTotal path length = " << cities_count << endl;
+			cout << "\nTotal path length = " << network->get_pq().top()->est_cities - 1 << endl;
 		else
 			cout << "\nTotal path length = " << total_distance << endl; 
 	}
